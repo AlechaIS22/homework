af1b91e134e3ecafec78f16345ca193b2d7b2973
ALeks <alechazaxarov@mail.ru>
2025-01-18 16:03:08 +0300

    Cоздание файла

---
+++ methodeSumNumberinObj.js
@@ -0,0 +1,15 @@
+let methodObject = {
+    sumNumberInObject() {
+
+    },
+    objHsaNumberInArray(){
+
+    },
+};
+
+let testObject = {
+    name: 'Ole',
+    friends: 5,
+    likes: 19,
+    projects: 7,
+};
