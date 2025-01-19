ca8e9a9e5c13c71a195ebea8047bae1294266e79
ALeks <alechazaxarov@mail.ru>
2025-01-19 14:16:53 +0300

    Выполнение 11 задания

---
+++ string.js
@@ -0,0 +1,32 @@
+let myMethods = {
+    firstSymbolUp (string) {
+        return string[0].toUpperCase() + string.slice(1);
+    },
+
+    stringClipping (string, limitation = 40) {
+        if (string.length <= limitation) return string
+        else return string.slice(0, string.lastIndexOf(" ", 40)) + "...";
+    },
+
+    isSubstring (firstString, secondString) {
+        switch (firstString.localeCompare(secondString)) {
+            case -1:
+                return secondString.indexOf(firstString) >= 0;
+            case 1:
+                return firstString.indexOf(secondString) >= 0;
+            default:
+                return true;
+        }
+    },
+};
+stringTest = "qwerty qwerty; qwerty, qwerty. qwertyaa! qwerty ";
+substring = "qwerty";
+noSubString = "123";
+console.log(myMethods.firstSymbolUp(stringTest));
+console.log(myMethods.stringClipping(stringTest));
+console.log(myMethods.isSubstring(stringTest, substring),
+    myMethods.isSubstring(stringTest, noSubString), 
+    myMethods.isSubstring(stringTest, stringTest),
+    myMethods.isSubstring(substring, stringTest), 
+    myMethods.isSubstring(noSubString, stringTest),
+);
